dbcaeb26b7c744cb1ce6139575db9068db187043
Miguel <242977+Miggleness@users.noreply.github.com>
2019-09-03 00:54:13 +0800

    almost done with phase 1

---
+++ src/MarvelCards/MarvelCards/HeroCard.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MarvelCards
+{
+    public partial class HeroCard : ContentView
+    {
+        public HeroCard()
+        {
+            InitializeComponent();
+        }
+
+        public Image CardImage => HeroImage;
+        
+    }
+}
--- src/MarvelCards/MarvelCards/MainPage.xaml.cs
+++ src/MarvelCards/MarvelCards/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,80 @@ namespace MarvelCards
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        const double _defaultTranslationY = -150;
+
         public MainPage()
         {
             InitializeComponent();
             base.BindingContext = new HeroCardsViewModel();
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MainCardView.UserInteracted += MainCardView_UserInteracted;            
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MainCardView.UserInteracted -= MainCardView_UserInteracted;
+
+        }
+
+        private void MainCardView_UserInteracted(PanCardView.CardsView view,
+            PanCardView.EventArgs.UserInteractedEventArgs args)
+        {
+            var card = MainCardView.CurrentView as HeroCard;
+            var nextCard = MainCardView.CurrentBackViews.FirstOrDefault() as HeroCard;
+
+            var percentFromCenter = Math.Abs(args.Diff / this.Width);
+            Debug.WriteLine($"% from C: {card.CardImage.Source.ToString()} {args.Status} {percentFromCenter}");
+
+            if (nextCard != null)
+            {
+                Debug.WriteLine($"Next: {nextCard.CardImage.Source.ToString()}");
+            }
+
+            if (args.Status == PanCardView.Enums.UserInteractionStatus.Started)
+            {
+                if (nextCard != null)
+                {
+                    nextCard.Opacity = 1;
+                    nextCard.CardImage.Scale = 1;
+                    nextCard.CardImage.TranslationY = _defaultTranslationY;
+                }
+            }
+
+            if (args.Status == PanCardView.Enums.UserInteractionStatus.Running)
+            {
+                // control opacity of currnet card
+                var opacity = 1 - (percentFromCenter * 0.8);
+                card.Opacity = (opacity > 1) ? 1 : opacity;
+                
+
+                // set scale of image in current card
+                card.CardImage.Scale = Math.Max(1 - (percentFromCenter * 1.5), .5);
+
+                // set position of image in current card
+                var movementFactor = 150;
+                card.CardImage.TranslationY = _defaultTranslationY + (movementFactor * percentFromCenter);
+
+                // set opacity of next card
+                nextCard.CardImage.Opacity = 1 - (opacity/4);
+                nextCard.CardImage.Scale = Math.Min(percentFromCenter * 3, 1);
+            }
+
+            if(args.Status == PanCardView.Enums.UserInteractionStatus.Ended
+                || args.Status == PanCardView.Enums.UserInteractionStatus.Ending)
+            {
+                card.Opacity = 1;
+                card.CardImage.Opacity = 1;
+                card.CardImage.Scale = 1;
+                card.CardImage.TranslationY = _defaultTranslationY;
+            }
+        }
+
+        
     }
 }
